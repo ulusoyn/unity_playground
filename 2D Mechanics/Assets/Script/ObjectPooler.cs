042295cb3299f4a2f3a8fe77fdba94908666f0cb
nuri <ulusoynuri3@gmail.com>
2025-02-28 14:40:30 +0300

    shoot function is given functionality
    -added fire command (gave the bullets an initial velocity when they are shot)
    -a circle should be added outside the character where a gameobject or a tool will be aligned to the perimeter of the circle
    - the tool will constantly rotate around the player with respect to the mouse position
    - the tool will be the initial position of the bullets

---
+++ 2D Mechanics/Assets/Script/ObjectPooler.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool2D (string tag, Vector2 position, quaternion rotation) // for 3d, use vector3 for position and quaternion for rotation
+    {
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " does not exist");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        IPooledObject pooledObj = objectToSpawn.GetComponent<IPooledObject>();
+
+        if (pooledObj != null)
+        {
+            pooledObj.OnObjectSpawn();
+        }
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+}
--- 2D Mechanics/Assets/Script/Weapon.cs
+++ 2D Mechanics/Assets/Script/Weapon.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Weapon : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Transform firePoint;
+    public float bulletVel = 10f;
+    Vector3 mousePos;
+    private Camera mainCam;
     public GameObject bulletPrefab;
 
+    ObjectPooler objectPooler;
 
     // Update is called once per frame
     void Update()
@@ -16,9 +21,29 @@ public class Weapon : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
     void Shoot()
     {
         // shooting logic
-        // Instantiate(bulletPrefab, );// to spawn an object, Instantiate is used
+        // Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);// to spawn an object, Instantiate is used
+        bulletPrefab = objectPooler.SpawnFromPool2D("Bullet", firePoint.position, firePoint.rotation);
+            
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = mousePos - firePoint.position;
+
+        Vector2 unit_direction = direction / direction.magnitude;
+
+        Debug.Log("bullet vector: " + direction);
+        Debug.Log("vector length: " + direction.magnitude);
+
+        Rigidbody2D bullet = bulletPrefab.GetComponent<Rigidbody2D>();
+        
+        bullet.linearVelocity = unit_direction*bulletVel;
     }
 }
